4434f5a5a1862c063a04217f5ef9d08b2f302579
MANGLAM TRIPATHI <58903148+manglam-tripathi@users.noreply.github.com>
2022-02-28 01:56:25 +0530

    Add files via upload

---
+++ MinMax.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class MinMax {
+	public static void main(String[] args) {
+		//length=5;
+		arr=[1,3,5,7,9]
+		miniMiniSum (Sum of 4 out of 5 elements) 
+		miniMaxSum	
+		int min=arr[0];
+		int max=arr[0];
+		int totalSum=0;
+		
+	}
+
+}
